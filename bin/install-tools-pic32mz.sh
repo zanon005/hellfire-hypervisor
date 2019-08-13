334eb824211d03e07c27a73d0eecf752f6b0c3fb
Carlos Moratelli <crmoratelli@gmail.com>
2019-08-13 12:31:48 -0300

    New fix on toolchain URL path

--- bin/install-tools-pic32mz.sh
+++ bin/install-tools-pic32mz.sh
@@ -16,7 +16,7 @@
 
 
 URL_MPLABIPE=http://ww1.microchip.com/downloads/en/DeviceDoc/MPLABX-v4.05-linux-installer.tar
-URL_CODESCAPE=https://www.mips.com/?do-download=linux-x64-mti-bare-metal-2016-05-06
+URL_CODESCAPE=https://www.dropbox.com/s/aescc1ouoqzrc8f/microchip_tools.tar.xz?dl=0
 MPLABIPE=MPLABX-v4.05-linux-installer.tar
 CODESCAPE=microchip_tools.tar.xz
 DOWNLOAD_DIR=~/Downloads/hyper_files
@@ -68,8 +68,8 @@ if [ ! -f "$CODESCAPE" ]; then
 	download_codescape;
 fi;
 
-tar -xvf $MPLABIPE
-./MPLABX-v4.05-linux-installer.sh
+#tar -xvf $MPLABIPE
+#./MPLABX-v4.05-linux-installer.sh
 
 tar -xvf $CODESCAPE
 mv microchip/mips-mti-elf /opt
