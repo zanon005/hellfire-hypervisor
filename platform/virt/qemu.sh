8719f43f81589e60fe1e4aae3f3a66ab28d89a5f
Carlos Moratelli <carlos.moratelli@ufsc.br>
2019-09-16 16:04:28 -0300

    Modify the linker script.

--- platform/virt/qemu.sh
+++ platform/virt/qemu.sh
@@ -1,1 +1,12 @@
- ~/qemu/qemu-4.1.0/riscv64-softmmu/qemu-system-riscv64 -M virt -nographic -kernel prplHypervisor.elf -serial mon:stdio
+ 
+#!/bin/sh
+
+if "$1" = "start"; than
+	~/qemu/qemu-4.1.0/riscv64-softmmu/qemu-system-riscv64 -M virt -nographic -kernel prplHypervisor.elf -serial mon:stdio -s -S &
+elif "$1" = "debug"; than
+	~/qemu/qemu-4.1.0/riscv64-softmmu/qemu-system-riscv64 -M virt -nographic -kernel prplHypervisor.elf -serial mon:stdio &
+elif "$1" = "kill"; than
+	killall qemu-system-riscv64
+fi
+
+
