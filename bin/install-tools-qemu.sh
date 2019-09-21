1ab1888e988b60c80e394858b3e55d1456af0ef7
Carlos Moratelli <crmoratelli@gmail.com>
2019-09-20 23:13:52 -0300

    Improve install script for RISCV tools.

--- bin/install-tools-qemu.sh
+++ bin/install-tools-qemu.sh
@@ -1,9 +1,13 @@
-TOOLS_DIR=$HOME/riscv-tools
+OOLS_DIR=$HOME/riscv-tools
 
 # Install required packages
-sudo apt-get install autoconf automake autotools-dev curl \
+sudo apt update
+sudo apt upgrade
+sudo apt install autoconf automake autotools-dev curl \
   		libmpc-dev libmpfr-dev libgmp-dev gawk build-essential \
-  		bison flex texinfo gperf libtool patchutils bc zlib1g-dev
+  		bison flex texinfo gperf libtool patchutils bc zlib1g-dev pkg-config \
+		libglibmm-2.4-dev libpixman-1-dev libconfig-dev
+
 
 mkdir -p $TOOLS_DIR
 pushd $TOOLS_DIR
